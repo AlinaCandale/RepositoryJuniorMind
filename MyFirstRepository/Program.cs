9a4996a6c7834d824501e0e5f9131d715c8ac13f
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-01-24 15:25:41 +0200

    Second commit

--- MyFirstRepository/Program.cs
+++ MyFirstRepository/Program.cs
@@ -7,6 +7,7 @@ namespace MyFirstRepository
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Junior Mind!");
         }
     }
 }
