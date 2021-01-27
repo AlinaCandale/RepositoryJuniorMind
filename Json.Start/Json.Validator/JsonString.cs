e7b2294ba9f20afa2ffddaa8c9356241624b64f1
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-01-27 17:50:21 +0200

    add code for string is not null

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -6,6 +6,11 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
             return input.StartsWith('"') && input.EndsWith('"');
         }
     }
