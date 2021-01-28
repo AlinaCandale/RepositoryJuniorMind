acba51f0ef295eac939df793a79b6e556a6f0635
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-01-28 14:02:27 +0200

    I refactorized the written code so far

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -6,12 +6,17 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
-            if (string.IsNullOrWhiteSpace(input))
-            {
-                return false;
-            }
+            return IsNullOrWhiteSpace(input) && IsDoubleQuoted(input);
+        }
 
+        static bool IsDoubleQuoted(string input)
+        {
             return input.StartsWith('"') && input.EndsWith('"');
+        }
+
+        static bool IsNullOrWhiteSpace(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
         }
     }
 }
