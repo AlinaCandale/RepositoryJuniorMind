f18e7e768bd008c7e9133c0efa4529a078db9330
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-02-03 16:20:17 +0200

    test DoesNotHaveTwoExponents passed

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -9,11 +9,16 @@ namespace Json
             return IsNullOrWhiteSpace(input)
                 && ContainLetters(input)
                 && StartWithZero(input)
-                && ContainDot(input);
+                && ContainCertainValue(input, '.');
         }
 
         static bool ContainLetters(string input)
         {
+            if (!ContainCertainValue(input, 'e'))
+            {
+                return false;
+            }
+
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] != 'e' && input[i] != 'E' && char.IsLetter(input[i]))
@@ -49,15 +54,15 @@ namespace Json
             return true;
         }
 
-        static bool ContainDot(string input)
+        static bool ContainCertainValue(string input, char value)
         {
-            if (!input.Contains('.'))
+            if (!input.Contains(value))
             {
                 return true;
             }
 
-            int freq = input.Split('.').Length - 1;
-            return freq == 1 && input.IndexOf('.') != input.Length - 1;
+            int freq = input.Split(value).Length - 1;
+            return freq == 1 && input.IndexOf(value) != input.Length - 1;
         }
     }
 }
