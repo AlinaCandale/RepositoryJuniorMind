3351af1bef99a42cff096ca70742534ed18e180d
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-02-04 11:26:18 +0200

    I refactorized method IsValidEscapeCharacter

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -39,19 +39,19 @@ namespace Json
 
         static bool IncludesOnlyRecognizedCharacters(string input)
         {
-            const int four = 4;
-            const int two = 2;
+            const int unicodeCharacterLength = 4;
+            const int pozToJump = 2;
 
             for (int i = 0; i < input.Length - 1; i++)
             {
                 if (input[i] == '\\')
                 {
-                    if (input[i + 1] == 'u' && input.Length - 1 - i >= four)
+                    if (input[i + 1] == 'u' && input.Length - 1 - i >= unicodeCharacterLength)
                     {
-                        return CheckIfCharIsHexadecimal(input, i + two);
+                        return CheckIfCharIsHexadecimal(input, i + pozToJump);
                     }
 
-                    return CheckIfCharIsContainByValues(input[i + 1]);
+                    return IsValidEscapeCharacter(input[i + 1]);
                 }
 
                 if (input[input.Length - 1 - 1] == '\\')
@@ -63,25 +63,17 @@ namespace Json
             return true;
         }
 
-        static bool CheckIfCharIsContainByValues(char character)
+        static bool IsValidEscapeCharacter(char character)
         {
-            char[] values = { '"', '\\', '/', 'f', 'b', 'n', 'r', 't' };
-            for (int j = 0; j < values.Length; j++)
-            {
-                if (character == values[j])
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            const string values = "\"\\/fbnrt";
+            return values.Contains(character);
         }
 
         static bool CheckIfCharIsHexadecimal(string input, int index)
         {
-            const int four = 4;
+            const int unicodeCharacterLength = 4;
             int count = 0;
-            for (int i = index; i < (index + four); i++)
+            for (int i = index; i < (index + unicodeCharacterLength); i++)
             {
                 if (Uri.IsHexDigit(input[i]))
                 {
@@ -89,7 +81,7 @@ namespace Json
                 }
             }
 
-            return count == four;
+            return count == unicodeCharacterLength;
         }
     }
 }
