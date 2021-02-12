5184a7c66ad0ccf4ab67b6e0cd009a0a243a177e
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-02-12 14:04:01 +0200

    i refactorized method SortTeamsByPoints

--- Football/Football/Ranking.cs
+++ Football/Football/Ranking.cs
@@ -46,17 +46,22 @@ namespace Football
 
         void SortTeamsByPoints()
         {
-            for (int j = 0; j <= list.Length - 2; j++)
+            for (int i = 0; i < list.Length - 1; i++)
             {
-
-                for (int i = 0; i <= list.Length - 2; i++)
+                bool swapped = false;
+                for (int j = i + 1; j < list.Length; j++)
                 {
-                    if (list[i].CompareTeamsPoints(list[i + 1]))
+                    if (list[j - 1].CompareTeamsPoints(list[j]))
                     {
-                        FootballTeam temp = list[i];
-                        list[i] = list[i + 1];
-                        list[i + 1] = temp;
+                        FootballTeam temp = list[j];
+                        list[j] = list[j - 1];
+                        list[j - 1] = temp;
+                        swapped = true;
                     }
+                }
+                if (!swapped)
+                {
+                    break;
                 }
             }
         }
