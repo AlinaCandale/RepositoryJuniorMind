d8504f032e8a7def24f238a189504340eb9c7cf5
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-02-24 12:08:06 +0200

    i add class Many | ManyFacts and works

---
+++ JsonSecondPart/JsonSecondPart.Facts/ManyFacts.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace JsonSecondPart.Facts
+{
+    public class ManyFacts
+    {
+        [Fact]
+        public void CheckIfRemainingTextIsCorrectIfManyConteinsCharacter()
+        {
+            var a = new Many(new Character('a'));
+
+            Assert.Equal("bc", a.Match("abc").RemainingText()); // true  
+            Assert.Equal("bc", a.Match("aaaabc").RemainingText()); // true 
+            Assert.Equal("bc", a.Match("bc").RemainingText()); // true 
+            Assert.Equal("", a.Match("").RemainingText()); // true 
+            Assert.Equal(null, a.Match(null).RemainingText()); // true 
+        }
+
+        [Fact]
+        public void CheckIfRemainingTextIsCorrectIfManyConteinsRange()
+        {
+            var digits = new Many(new Range('0', '9'));
+
+            Assert.Equal("ab123", digits.Match("12345ab123").RemainingText()); // true 
+            Assert.Equal("ab", digits.Match("ab").RemainingText()); // true  
+        }
+    }
+}
---
+++ JsonSecondPart/JsonSecondPart/Many.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonSecondPart
+{
+    public class Many : IPattern
+    {
+        IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            string remainingText = text;
+
+            while (pattern.Match(remainingText).Success())
+            {
+                remainingText = remainingText.Substring(1);
+            }
+            
+            return new SuccessMatch(remainingText);
+        }
+    }
+}
