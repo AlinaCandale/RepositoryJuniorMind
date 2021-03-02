c09dcfd26fc2c356ebce5ffc18e91605030aa99f
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-03-02 13:49:50 +0200

    Class OneOrMore work properly

--- JsonSecondPart/JsonSecondPart/OneOrMore.cs
+++ JsonSecondPart/JsonSecondPart/OneOrMore.cs
@@ -10,18 +10,13 @@ namespace JsonSecondPart
 
         public OneOrMore(IPattern pattern)
         {
-            // aici folosește-te de clasele implementate deja
-            // pentru a construi un pattern care să îl folosești în Match
-            this.pattern = new Many(pattern);
+            //this.pattern = new Many(pattern);
+            this.pattern = new Sequence(pattern, new Many(pattern));
         }
 
         public IMatch Match(string text)
         {
-            if (text == pattern.Match(text).RemainingText())
-            {
-                return new FailedMatch(text);
-            }
-
+            //if (text == pattern.Match(text).RemainingText()) {return new FailedMatch(text);}
             return pattern.Match(text);
         }
     }
