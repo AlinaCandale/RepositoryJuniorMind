dc899335f6c74b73ca6c42bae8104ae23a3c0d3c
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-03-11 16:04:59 +0200

    I add class Number | NumberFacts

--- JsonSecondPart/JsonSecondPart/List.cs
+++ JsonSecondPart/JsonSecondPart/List.cs
@@ -10,11 +10,7 @@ namespace JsonSecondPart
 
         public List(IPattern element, IPattern separator)
         {
-            // aici folosește-te de clasele implementate deja
-            // pentru a construi un pattern pe care să îl folosești în Match
-            //this.pattern = new Many(new Choice(element, new Sequence(separator, element)));
             this.pattern = new Optional(new Sequence(element, new Many(new Sequence(separator,element))));
-
         }
 
         public IMatch Match(string text)
---
+++ JsonSecondPart/JsonSecondPart/Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonSecondPart
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+        private readonly IPattern firstChar;
+        private readonly IPattern numberWithDot;
+        private readonly IPattern numberWithExponent;
+
+        public Number()
+        {   
+            firstChar = new Choice(new Character('-'), new Character('0'), new Range('1', '9'));
+            numberWithExponent = new List(new Many(new Range('0', '9')), new Choice(new Character('e'), new Character('E'), new Text("e+"), new Text("e-"), new Text("E+"), new Text("E-")));
+            numberWithDot = new Sequence(new Character('.'), new Choice(new Many(new Range('0', '9')), numberWithExponent));
+
+            pattern = new Choice(
+                firstChar, 
+                new Character(' '),
+                new Many(new Range('1', '9')),
+                numberWithDot,
+                numberWithExponent
+                    );
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
