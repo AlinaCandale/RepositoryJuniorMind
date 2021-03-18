c9e79d5e61da4f9efd64c45ad9fe982dbd062995
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-03-18 15:20:41 +0200

    i refactorised NumberFacts

--- JsonSecondPart/JsonSecondPart.Facts/NumberFacts.cs
+++ JsonSecondPart/JsonSecondPart.Facts/NumberFacts.cs
@@ -5,6 +5,30 @@ namespace JsonSecondPart.Facts
 {
     public class NumberFacts
     {
+        [Theory]
+        [InlineData("abc", "abc")]
+        [InlineData("0", "")]
+        [InlineData("1", "")]
+        [InlineData("0.7", "")]
+        [InlineData("07", "7")]
+        [InlineData("12.", ".")]
+        [InlineData("12e3", "")]
+        [InlineData("12E2", "")]
+        [InlineData("1e+2", "")]
+        [InlineData("2+", "+")]
+        [InlineData("3e-", "e-")]
+        [InlineData("123.e", ".e")]
+        [InlineData("123.1e", "e")]
+        [InlineData("12e3.1e", ".1e")]
+        [InlineData("123.1eE", "eE")]
+
+        public void NumberConsumesTheMatchingText(string input, string expectedRemainingText)
+        {
+            var a = new Number();
+            var result = a.Match(input);
+            Assert.Equal(expectedRemainingText, result.RemainingText());
+        }
+
         [Fact]
         public void JsonNumberCanBe()
         {
@@ -14,24 +38,17 @@ namespace JsonSecondPart.Facts
             Assert.True(a.Match("0").Success()); // true 
             Assert.True(a.Match("1").Success()); // true 
             Assert.True(a.Match("0.7").Success()); // true 
-            //Assert.False(a.Match("07").Success()); // false 
             Assert.Equal("7", a.Match("07").RemainingText());
-            //Assert.False(a.Match("12.").Success()); // false 
             Assert.Equal(".", a.Match("12.").RemainingText());
             Assert.True(a.Match("12e3").Success()); // true 
             Assert.True(a.Match("12E3").Success()); // true 
             Assert.True(a.Match("12e+3").Success()); // true 
             Assert.True(a.Match("12E-3").Success()); // true
-            //Assert.False(a.Match("12e+").Success()); // false
             Assert.Equal("e+", a.Match("12e+").RemainingText());
-            //Assert.False(a.Match("12E").Success()); // false
             Assert.Equal("E", a.Match("12E").RemainingText()); 
             Assert.True(a.Match("12.2E3").Success()); // true
             Assert.True(a.Match("12e3.2").Success()); // true
-            //Assert.False(a.Match("12e12E").Success()); // false 
             Assert.Equal("E", a.Match("12e12E").RemainingText());
         }
-
-
     }
 }    
