d8f34ef787bd5de752749dbfe7f0174e00e5e524
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-03-18 12:55:46 +0200

    test 1 for Class Number

--- JsonSecondPart/JsonSecondPart.Facts/NumberFacts.cs
+++ JsonSecondPart/JsonSecondPart.Facts/NumberFacts.cs
@@ -23,12 +23,15 @@ namespace JsonSecondPart.Facts
         public void JsonNumberCanBe()
         {
             var a = new Number();
-            
+
+            Assert.False(a.Match("abc").Success()); // false
             Assert.True(a.Match("0").Success()); // true 
             Assert.True(a.Match("1").Success()); // true 
             Assert.True(a.Match("0.7").Success()); // true 
-            Assert.False(a.Match("07").Success()); // false 
-            Assert.False(a.Match("12.").Success()); // false 
+            //Assert.False(a.Match("07").Success()); // false 
+            Assert.Equal("7", a.Match("07").RemainingText());
+            //Assert.False(a.Match("12.").Success()); // false 
+            Assert.Equal(".", a.Match("12.").RemainingText());
             Assert.True(a.Match("12e3").Success()); // true 
             Assert.True(a.Match("12E3").Success()); // true 
             Assert.True(a.Match("12e+3").Success()); // true 
--- JsonSecondPart/JsonSecondPart/Number.cs
+++ JsonSecondPart/JsonSecondPart/Number.cs
@@ -7,23 +7,28 @@ namespace JsonSecondPart
     public class Number : IPattern
     {
         private readonly IPattern pattern;
-        private readonly IPattern firstChar;
-        private readonly IPattern numberWithDot;
-        private readonly IPattern numberWithExponent;
-
+        
         public Number()
         {   
-            firstChar = new Choice(new Character('-'), new Character('0'), new Range('1', '9'));
-            numberWithExponent = new List(new Many(new Range('0', '9')), new Choice(new Character('e'), new Character('E'), new Text("e+"), new Text("e-"), new Text("E+"), new Text("E-")));
-            numberWithDot = new Sequence(new Character('.'), new Choice(new Many(new Range('0', '9')), numberWithExponent));
+            //var firstChar = new Choice(new Character('-'), new Character('0'), new Range('1', '9'));
+            //var numberWithExponent = new List(new Many(new Range('0', '9')), new Choice(new Character('e'), new Character('E'), new Text("e+"), new Text("e-"), new Text("E+"), new Text("E-")));
+            //var numberWithDot = new Sequence(new Character('.'), new Choice(new Many(new Range('0', '9')), numberWithExponent));
 
-            pattern = new Choice(
-                firstChar, 
-                new Character(' '),
-                new Many(new Range('1', '9')),
-                numberWithDot,
-                numberWithExponent
-                    );
+            //pattern = new Choice(
+            //    firstChar, 
+            //    new Character(' '),
+            //    new Many(new Range('1', '9')),
+            //    numberWithDot,
+            //    numberWithExponent
+            //        );
+
+            var digit = new Range('0', '9');
+            var digits = new Range('1', '9');
+            var integer = new Sequence(new Optional(new Character('-')), 
+                new Choice(new Character('0'), new Sequence(digits, new Optional(new Many(digit))))); 
+            var fraction = new Optional(new Sequence(new Character('.'), new Optional(new Many(digit))));
+            var exponent = new Optional(new Sequence(new Choice(new Text("e+"), new Text("e-"), new Text("E+"), new Text("E-"), new Any("eE")), new Many(digit)));
+            this.pattern = new Sequence(integer, fraction, exponent);
         }
 
         public IMatch Match(string text)
