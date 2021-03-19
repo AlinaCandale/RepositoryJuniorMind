eba8da2cbf7c6056e3f597b3052b5fca94ff1e3b
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-03-19 17:39:12 +0200

    second try class String

--- JsonSecondPart/JsonSecondPart/String.cs
+++ JsonSecondPart/JsonSecondPart/String.cs
@@ -10,14 +10,18 @@ namespace JsonSecondPart
 
         public String()
         {
-            var character = new Choice(new Range('\u0020', '\u0021'), new Range('\u0023', '\u005B'), new Range('\u005D', char.MaxValue));
-            var reverseSolidus = new Character('\u0022');
+            var backSlash = new Character('\u005C');
             var quotationMark = new Character('"');
-            var escape = new Sequence(reverseSolidus, new Any("\"\\/fbnrt"));
             var hex = new Choice(new Range('0', '9'), new Range('a', 'f'), new Range('A', 'F'));
-            var hexSeq = new Sequence(reverseSolidus, new Character('u'), new Sequence(hex, hex, hex, hex));
+            
+            var hexSeq = new Sequence(backSlash, new Character('u'), new Sequence(hex, hex, hex, hex));
+            var escape = new Choice(new Sequence(backSlash, new Any("\"\\/fbnrt")), hexSeq);
+            var character = new Choice(new Range('\u0020', '\u0021'), 
+                new Range('\u0023', '\u005B'), 
+                new Range('\u005D', char.MaxValue),
+                escape);
 
-            pattern = new Sequence(quotationMark, new Optional(new Choice(new OneOrMore(escape), new OneOrMore(hexSeq), new OneOrMore(character))), quotationMark);
+            pattern = new Sequence(quotationMark, new Optional(new OneOrMore(character)), quotationMark);
         }
 
         public IMatch Match(string text)
