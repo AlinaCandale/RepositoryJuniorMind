941b7460a6b1f3725fdedbef9b80b6e1bfc504a4
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-04-01 15:48:18 +0300

    I add Class MainClass and five TestFiles

---
+++ JsonSecondPart/JsonSecondPart/MainClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JsonSecondPart
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in args)
+            {
+                string text = System.IO.File.ReadAllText(@item);
+
+                var a = new Value();
+                if (a.Match(text).Success())
+                {
+                    Console.WriteLine("You have a valid Json value");
+                }
+                else
+                {
+                    Console.WriteLine("You don't have a valid Json value");
+                }
+            }
+
+        }
+    }
+
+}
