0c5c74f16029a0f2523377c87ea427b2be85fab4
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-04-14 15:06:08 +0300

    I replaced counter with Count and Count becomes autoproperty

--- classIntArray/IntArray/IntArray.cs
+++ classIntArray/IntArray/IntArray.cs
@@ -5,7 +5,7 @@ namespace Arrays
     public class IntArray
     {
         int[] intArray;
-        int counter = 0;
+        public int Count { get; private set; } = 0;
 
         public IntArray()
         {
@@ -15,25 +15,13 @@ namespace Arrays
         public void Add(int element)
         {
             ResizeArray();
-            intArray[counter] = element;
-            counter++;
-        }
-
-        public int Count
-        {
-            get
-            {
-                return counter;
-            }
-            private set
-            {
-                counter = value;
-            }
+            intArray[Count] = element;
+            Count++;
         }
 
         public int this[int index] //Element() + SetElement()
         {
-            get => counter > 0 && index < counter ? intArray[index] : -1;
+            get => Count > 0 && index < Count ? intArray[index] : -1;
             set => intArray[index] = value;
         }
 
@@ -44,7 +32,7 @@ namespace Arrays
 
         public int IndexOf(int element)
         {
-            return Array.IndexOf(intArray, element, 0, counter);
+            return Array.IndexOf(intArray, element, 0, Count);
         }
 
         public void Insert(int index, int element)
@@ -52,12 +40,12 @@ namespace Arrays
             ResizeArray();
             MoveElementsToRight(index, element);
             intArray[index] = element;
-            counter++;
+            Count++;
         }
 
         private void ResizeArray()
         {
-            if (counter >= intArray.Length)
+            if (Count >= intArray.Length)
             {
                 Array.Resize(ref intArray, intArray.Length * 2);
             }
@@ -65,7 +53,7 @@ namespace Arrays
 
         private void MoveElementsToRight(int index, int element)
         {
-            for (int i = counter; i >= index; i--)
+            for (int i = Count; i >= index; i--)
             {
                 intArray[i] = intArray[i - 1];
             }
@@ -74,7 +62,7 @@ namespace Arrays
         public void Clear()
         {
             intArray = new int[4];
-            counter = 0;
+            Count = 0;
         }
 
         public void Remove(int element)
@@ -85,11 +73,11 @@ namespace Arrays
 
         public void RemoveAt(int index)
         {
-            for (int i = index; i < counter - 1; i++)
+            for (int i = index; i < Count - 1; i++)
             {
                 intArray[i] = intArray[i + 1];
             }
-            counter--;
+            Count--;
         }
     }
 }
