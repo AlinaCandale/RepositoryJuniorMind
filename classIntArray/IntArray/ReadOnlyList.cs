01a4ffc74e034ccf27862fb262f1cdf53a155aa3
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-05-19 17:30:37 +0300

    i commit ReadOnlyList

---
+++ classIntArray/IntArray/ReadOnlyList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntArray
+{
+    public class ReadOnlyList<T> : IList<T>
+    {
+        readonly IList<T> _list = new List<T>();
+
+        public T this[int index] { get => _list[index]; set => throw new NotSupportedException("List is read-only"); }
+
+        public int Count => _list.Count;
+
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotSupportedException("List is read-only");
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException("List is read-only");
+        }
+
+        public bool Contains(T item)
+        {
+            return _list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _list.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            return _list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotSupportedException("List is read-only");
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotSupportedException("List is read-only");
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotSupportedException("List is read-only");
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_list).GetEnumerator();
+        }
+    }
+}
