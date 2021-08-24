9d3a53b2c9438dcc70095b777a5013c69ac08de2
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-08-24 12:33:02 +0300

    i refactorised the exercise using linq

--- CountVowelsConsonants/CountVowelsConsonants.Facts/UnitTest1.cs
+++ CountVowelsConsonants/CountVowelsConsonants.Facts/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace CountVowelsConsonants.Facts
     public class UnitTest1
     {
         [Fact]
-        public void CheckVowels()
+        public void CheckVowelsAndConsonates()
         {
             string text = "The quick brown fox jumps over the lazy dog.";
             CountLetters sentance = new CountLetters(text);
@@ -16,7 +16,28 @@ namespace CountVowelsConsonants.Facts
 
             Assert.Equal(11, vowelsNr);
             Assert.Equal(24, consonantNr);
+        }
 
+        [Fact]
+        public void CheckIfVowelsIsZero()
+        {
+            string text = "Th qck brwng.";
+            CountLetters sentance = new CountLetters(text);
+
+            int vowelsNr = sentance.GetNrOfVowels();
+
+            Assert.Equal(0, vowelsNr);
+        }
+
+        [Fact]
+        public void CheckIfConsonatesIsZero()
+        {
+            string text = "    aaaa !";
+            CountLetters sentance = new CountLetters(text);
+
+            int consonantNr = sentance.GetNrOfConsonant();
+
+            Assert.Equal(0, consonantNr);
         }
     }
 }
--- CountVowelsConsonants/CountVowelsConsonants/CountLetters.cs
+++ CountVowelsConsonants/CountVowelsConsonants/CountLetters.cs
@@ -7,56 +7,20 @@ namespace CountVowelsConsonants
     public class CountLetters
     {
         string sentence;
-        int vowelCount;
-        int consonantCount;
 
         public CountLetters(string sentence)
         {
             this.sentence = sentence;
-            CountVowelsandCOnsonants();
         }
 
         public int GetNrOfVowels()
         {
-            return vowelCount;
+            return sentence.Count(c => "aeiou".Contains(Char.ToLower(c)));
         }
 
         public int GetNrOfConsonant()
         {
-            return consonantCount;
-        }
-        
-        void CountVowelsandCOnsonants()
-        {
-            foreach (char c in sentence.ToLower())
-            {
-                if (Char.IsLetter(c))
-                {
-                    if ("aeiou".Contains(c))
-                    {
-                        vowelCount++;
-                    }
-                    else
-                    {
-                        consonantCount++;
-                    }
-                }
-            }
+            return sentence.Where(c => Char.IsLetter(c)).Count(c => !"aeiou".Contains(Char.ToLower(c)));
         }
     }
 }
-//int vowelCount = sentence.Count(c => "aeiou".Contains(Char.ToLower(c)));
-
-//int letterCount = sentence.Count(c => Char.IsLetter(c));
-//int vowelCount = sentence.Count(c => "aeiou".Contains(Char.ToLower(c)));
-//string test = "abbbb";
-//char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
-//var result = test.ToCharArray().Where(c => vowels.Contains(c));
-//Console.WriteLine(result.Count());
-//Console.ReadLine();
-
-
-//foreach (var consonant in line.Where(charInString => !charArray.Contains(charInString)).Distinct())
-//{
-//    consonantCounts.Add(consonant, line.Count(charInString => consonant == charInString));
-//}
