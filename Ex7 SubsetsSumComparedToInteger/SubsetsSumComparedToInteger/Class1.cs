21cc7cb56a6c092f5affb5c57ab919bad4be6a1b
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-09-28 16:50:37 +0300

    i solved the Ex without linq methods

--- Ex7 SubsetsSumComparedToInteger/SubsetsSumComparedToInteger/Class1.cs
+++ Ex7 SubsetsSumComparedToInteger/SubsetsSumComparedToInteger/Class1.cs
@@ -15,48 +15,31 @@ namespace SubsetsSumComparedToInteger
             this.sum = sum;
         }
 
-        //public int[] AllSubsetsWithSumLessThanInt()
-        //{
-
-        //    //return Enumerable.Range(1, set.Length)
-        //    //    .SelectMany(length => Enumerable.Range(0, set.Length - length + 1).Select(i => set.Substring(i, length)))
-        //    //    .Where(item => item.SequenceEqual(item.Reverse()))
-        //    //    .ToArray();
-
-        //    //var r = Enumerable.Range(1, text.Length);
-        //    //var rr = r.SelectMany(length => Enumerable.Range(0, text.Length - length + 1).Select(i => text.Substring(i, length)));
-        //    //var rrr = rr.Where(item => item.SequenceEqual(item.Reverse()));
-        //    //var rrrr = rrr.ToArray();
-        //    //return rrrr;
-
-        //    //NU
-        //    //return (int[])Enumerable.Range(1, set.Length)
-        //    //    .SelectMany(length => Enumerable.Range(0, set.Length - length + 1).Select((i, length) => set.Sum()))
-        //    //    .Where(item => item <= nr)
-        //    //    .ToArray();
-
-        //    //var r = Enumerable.Range(1, set.Length - 1);
-        //    //var rr = r.SelectMany(length => Enumerable.Range(1, set.Length * set.Length - 1).Select((i, length) => set.Sum()));
-        //    //var rrr = rr.Where(item => item <= nr);
-        //    //var rrrr = rrr.ToArray();
-
-        //    //return rrrr;
-
-        //}
-
-        public static IEnumerable<T[]> GetCombinations<T>(IEnumerable<T> source)
+        public static void GetRequiredSumSubArray(int[] set, int sum)
         {
-            if (null == source)
-                throw new ArgumentNullException(nameof(source));
-
-            T[] data = source.ToArray();
-
-            return Enumerable.Range(1, (1 << (data.Length)) - 2)
-              .Select(index => data.Where((v, i) => (index & (1 << i)) != 0).ToArray());
+            List<int> subArray = new();
+            int temp = 0;
+            for (int i = 0; i < set.Length; i++)
+            {
+                for (int j = i, j < set.Length; j++)
+                {
+                    temp += set[j];
+                    subArray.Add(set[j]);
+                    
+                    if (temp == sum)
+                    {
+                        Console.WriteLine(string.Join(", ", subArray));
+                    }
+                    
+                    if (temp > sum)
+                    {
+                        subArray.Clear(); 
+                        temp = 0;
+                        break;
+                    }
+                }
+            }
         }
-
-
-
     }
 }
 //Pentru un șir de numere întregi și un număr întreg dat k generează toate subșirurile a căror sumă este mai mică sau egală cu k.
