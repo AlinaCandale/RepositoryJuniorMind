d5cb5cb1a1658517afdbd649ce6186af0f95980f
AlinaCandale <77732771+AlinaCandale@users.noreply.github.com>
2021-09-29 18:09:44 +0300

    i modified the solution, so now it only generates the subsets

--- Ex7 SubsetsSumComparedToInteger/SubsetsSumComparedToInteger.Facts/UnitTest1.cs
+++ Ex7 SubsetsSumComparedToInteger/SubsetsSumComparedToInteger.Facts/UnitTest1.cs
@@ -7,45 +7,60 @@ namespace SubsetsSumComparedToInteger.Facts
 {
     public class UnitTest1
     {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    int[] set = { 1, 2, 3 };
+        //    int sum = 5;
+        //    GetAllSubsets x = new GetAllSubsets(set,sum);
+        //    List<int> x1 = new List<int>() { 1 };
+        //    List<int> x2 = new List<int>() { 2 };
+        //    List<int> x3 = new List<int>() { 3 };
+        //    List<int> x4 = new List<int>() { 1, 2 };
+        //    List<int> x5 = new List<int>() { 2, 3 };
+        //    List<List<int>> result = new List<List<int>>() { x1, x4, x2, x5, x3 };
+
+        //    List<List<int>> expected = x.GetRequiredSumSubArray(set, sum);
+        //    Assert.Equal(result, expected);
+        //}
+
+        //[Fact]
+        //public void Test2()
+        //{
+        //    int[] set = { 1, 2, 3, 4 };
+        //    int sum = 10;
+        //    GetAllSubsets x = new GetAllSubsets(set, sum);
+        //    List<int> x1 = new List<int>() { 1 };
+        //    List<int> x2 = new List<int>() { 2 };
+        //    List<int> x3 = new List<int>() { 3 };
+        //    List<int> x4 = new List<int>() { 1, 2 };
+        //    List<int> x5 = new List<int>() { 2, 3 }; 
+        //    List<int> x6 = new List<int>() { 4 };
+        //    List<int> x7 = new List<int>() { 1, 2, 3 };
+        //    List<int> x8 = new List<int>() { 1, 2, 3, 4 };
+        //    List<int> x9 = new List<int>() { 2, 3, 4 };
+        //    List<int> x10 = new List<int>() { 3, 4 };
+        //    List<List<int>> result = new List<List<int>>() { x1, x4, x7, x8, x2, x5, x9, x3, x10, x6 };
+
+        //    List<List<int>> expected = x.GetRequiredSumSubArray(set, sum);
+        //    Assert.Equal(result, expected);
+        //}
+
         [Fact]
         public void Test1()
         {
             int[] set = { 1, 2, 3 };
-            int sum = 5;
-            GetAllSubsets x = new GetAllSubsets(set,sum);
+            GetAllSubsets x = new GetAllSubsets(set);
             List<int> x1 = new List<int>() { 1 };
             List<int> x2 = new List<int>() { 2 };
             List<int> x3 = new List<int>() { 3 };
             List<int> x4 = new List<int>() { 1, 2 };
             List<int> x5 = new List<int>() { 2, 3 };
-            List<List<int>> result = new List<List<int>>() { x1, x4, x2, x5, x3 };
+            List<int> x6 = new List<int>() { 1, 2, 3 };
+            List<List<int>> result = new List<List<int>>() { x1, x4, x6, x2, x5, x3 };
 
-            List<List<int>> expected = x.GetRequiredSumSubArray(set, sum);
+            List<List<int>> expected = x.GetRequiredSumSubArray(set);
             Assert.Equal(result, expected);
-
-        }
-
-        [Fact]
-        public void Test2()
-        {
-            int[] set = { 1, 2, 3, 4 };
-            int sum = 10;
-            GetAllSubsets x = new GetAllSubsets(set, sum);
-            List<int> x1 = new List<int>() { 1 };
-            List<int> x2 = new List<int>() { 2 };
-            List<int> x3 = new List<int>() { 3 };
-            List<int> x4 = new List<int>() { 1, 2 };
-            List<int> x5 = new List<int>() { 2, 3 }; 
-            List<int> x6 = new List<int>() { 4 };
-            List<int> x7 = new List<int>() { 1, 2, 3 };
-            List<int> x8 = new List<int>() { 1, 2, 3, 4 };
-            List<int> x9 = new List<int>() { 2, 3, 4 };
-            List<int> x10 = new List<int>() { 3, 4 };
-            List<List<int>> result = new List<List<int>>() { x1, x4, x7, x8, x2, x5, x9, x3, x10, x6 };
-
-            List<List<int>> expected = x.GetRequiredSumSubArray(set, sum);
-            Assert.Equal(result, expected);
-
         }
     }
 }
--- Ex7 SubsetsSumComparedToInteger/SubsetsSumComparedToInteger/Class1.cs
+++ Ex7 SubsetsSumComparedToInteger/SubsetsSumComparedToInteger/Class1.cs
@@ -7,40 +7,25 @@ namespace SubsetsSumComparedToInteger
     public class GetAllSubsets
     {
         int[] set;
-        int sum;
 
-        public GetAllSubsets(int[] set, int sum)
+        public GetAllSubsets(int[] set)//, int sum)
         {
             this.set = set;
-            this.sum = sum;
         }
 
-        public List<List<int>> GetRequiredSumSubArray(int[] set, int sum)
+        public List<List<int>> GetRequiredSumSubArray(int[] set)
         {
             List<int> subArray = new();
             List<List<int>> result = new();
 
             for (int i = 0; i < set.Length; i++)
             {
-                int temp = 0;
                 subArray.Clear();
 
                 for (int j = i; j < set.Length; j++)
                 {
-                    temp += set[j];
-                    
-                    if (temp <= sum)
-                    {
-                        subArray.Add(set[j]);
-                        result.Add(new List<int>(subArray));
-                    }
-                    
-                    if (temp > sum)
-                    {
-                        subArray.Clear(); 
-                        temp = 0;
-                        break;
-                    }
+                    subArray.Add(set[j]);
+                    result.Add(new List<int>(subArray));
                 }
             }
 
